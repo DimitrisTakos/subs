7101bf1c27dbb4a43441f68f3b5b2576d65a7651
dimtakos <dimitako@ihu.gr>
2024-08-13 00:28:15 +0300

    Progress site 20%

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    header = models.CharField(max_length=200, null=True)
+    progress = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(null=True)
+    text = models.CharField(max_length=2000, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    production_date = models.DateTimeField(blank=True, null=True)
+    duration = models.CharField(max_length=200, null=True)
+    grade = models.CharField(max_length=200, null=True)
+    cheatsheet = models.ForeignKey('Cheatsheet', null=True, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Cheatsheet(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='cheatsheets/', null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
