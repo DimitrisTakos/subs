a4cdbdb7c8d4d2052b979d8cdd8585903b4711b8
dimtakos <dimitako@ihu.gr>
2024-08-13 02:02:51 +0300

    Progress site 70%

--- movie/urls.py
+++ movie/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from movie import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.base, name='base'),
@@ -8,3 +10,6 @@ urlpatterns = [
          views.download_cheatsheet, name='download_cheatsheet'),
     path('old/', views.old, name='old'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
